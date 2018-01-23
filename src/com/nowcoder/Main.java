bbc3040742f1407ce7f11c4ee0008f7d0bcfc52e
Jason <1915933678@qq.com>
2018-01-23 16:31:07 +0800

    hello Git 测试

---
+++ src/com/nowcoder/Main.java
@@ -0,0 +1,10 @@
+package com.nowcoder;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello noecoder");
+        System.out.println("hello git 2222");
+    }
+}
